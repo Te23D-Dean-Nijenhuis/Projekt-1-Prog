e365935982af6a270820e2795da38a30a25c4e66
Dean Nijenhuis <Dean.Nijenhuis@elev.ga.ntig.se>
2025-01-20 09:13:14 +0100

    initierade raylib

---
+++ Projekt/Program.cs
@@ -0,0 +1,15 @@
+﻿using Raylib_cs;
+
+Raylib.InitWindow(800, 600, "The title of my window");
+Raylib.SetTargetFPS(60);
+
+while (!Raylib.WindowShouldClose())
+{
+  Raylib.BeginDrawing();
+  
+  Raylib.ClearBackground(Color.WHITE);
+  
+  Raylib.DrawCircle(100,100,100,Color.MAGENTA);
+  
+  Raylib.EndDrawing();
+}
