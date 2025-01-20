e64f8af46f0704d6ec5450af3d96767df87a83a6
Dean Nijenhuis <Dean.Nijenhuis@elev.ga.ntig.se>
2025-01-20 09:51:47 +0100

    övade med grafik loopar och metoder

--- Projekt/Program.cs
+++ Projekt/Program.cs
@@ -1,15 +1,43 @@
-﻿using Raylib_cs;
+﻿using System.Numerics;
+using System.Xml.Serialization;
+using Raylib_cs;
 
-Raylib.InitWindow(800, 600, "The title of my window");
+Raylib.InitWindow(800, 800, "The title of my window");
 Raylib.SetTargetFPS(60);
+
+int x = 0;
+int y = 390;
+int xh = 1;
+int yh = 1;
 
 while (!Raylib.WindowShouldClose())
 {
   Raylib.BeginDrawing();
   
-  Raylib.ClearBackground(Color.WHITE);
-  
-  Raylib.DrawCircle(100,100,100,Color.MAGENTA);
+  Raylib.ClearBackground(Color.White);
+
+  (x, y, xh, yh) = test(x, y, xh, yh);
   
   Raylib.EndDrawing();
+}
+
+static (int, int, int, int) test(int x, int y, int xh, int yh)
+{
+    Raylib.DrawRectangle(x,y,20,20,Color.Black);
+    x += xh;
+    y += yh;
+
+    if(y >= 780 || y <= 0)
+    {
+        yh *= -1;
+    }
+    if(x >= 780 || x <= 0)
+    {
+        xh *= -1;
+    }
+
+
+
+    return (x, y, xh, yh);
+
 }
