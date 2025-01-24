ef20997a701b49a2f544ac4e171c21eb0ad4e8cf
Dean Nijenhuis <Dean.Nijenhuis@elev.ga.ntig.se>
2025-01-24 11:07:14 +0100

    bla bla

--- Projekt/Program.cs
+++ Projekt/Program.cs
@@ -1,38 +1,29 @@
 ﻿using Raylib_cs;
 
-Raylib.InitWindow(800, 800, "The title of my window");
+Raylib.InitWindow(1600, 800, "Geomatry dashers");
 Raylib.SetTargetFPS(60);
+Color Background = new(0,225,0,255);
 
-int x = 10;
-int y = 100;
-int width = 50;
-int height = 50;
+List<Square> squares = [];
 
-List<int> coordinater = [];
 
-Rectangle fyra = new Rectangle (x,y,width,height);
 
 while (!Raylib.WindowShouldClose())
 {
   Raylib.BeginDrawing();
-  Raylib.ClearBackground(Color.Green);
+  
+  Raylib.ClearBackground(Background);
+  
 
 
-  ballong(x, y, width, height, fyra);
-
-  x++;
-
-
-
-
+  
   Raylib.EndDrawing();
 }
 
 
-static (int x, int y, int widht, int height) ballong(int x, int y, int width, int height, Rectangle fyra)
+static void Place()
 {
-  fyr
-  Raylib.DrawRectangle(x, y, width, height, Color.Red);
-  Raylib.DrawRectangleLinesEx(fyra, 5, Color.Black);
-  return (x,y,width,height);
+  Raylib.DrawRectangle(Square.posX, Square.posY, Square.width, Square.height)
+
 }
+
---
+++ Projekt/Square.cs
@@ -0,0 +1,7 @@
+public class Square
+{
+  public int posX;
+  public int posY;
+  public int width;
+  public int height;
+}
