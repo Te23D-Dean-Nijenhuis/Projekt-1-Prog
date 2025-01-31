7073e902d39d81615f85747047cef7fd520e3b1b
Dean Nijenhuis <Dean.Nijenhuis@elev.ga.ntig.se>
2025-01-31 14:34:25 +0100

    testar placement preview

--- Projekt/Program.cs
+++ Projekt/Program.cs
@@ -1,12 +1,18 @@
-﻿using Raylib_cs;
+﻿using System.Numerics;
+using Raylib_cs;
 
-Raylib.InitWindow(1600, 800, "Geomatry dashers");
+Raylib.InitWindow(1600, 800, "Geometry dashers");
 Raylib.SetTargetFPS(60);
 Color Background = new(0, 225, 0, 255);
-Color Red = new(255, 0, 0, 255);
+Color Red = new Color(255, 0, 0, 255);
 int counter = 0;
+bool yes = false;
+
+Color preview = new Color(0, 0, 255, 50);
 
 List<Square> squares = [];
+List<TOWER> TOWERS = [];
+
 
 
 while (!Raylib.WindowShouldClose())
@@ -15,9 +21,30 @@ while (!Raylib.WindowShouldClose())
 
   Raylib.ClearBackground(Background);
 
+  GRIDTEST();
+
+  TOWERPLACE(TOWERS); //placerar ut torn
+
+  TOWERDRAW(TOWERS); // ritar torn
+
+  Console.Clear(); //debug
+  Console.WriteLine(TOWERS.Count); //debug
+
+  if(Raylib.IsKeyPressed(KeyboardKey.T)||yes)
+  {
+    PREVIEW();
+    yes = true;
+
+  }
 
 
   
+
+
+
+
+
+
 
   /*if (counter == 0)
   {
@@ -47,3 +74,50 @@ static void Place(Square square)
   Raylib.DrawRectangleLinesEx(square.rect, 5, Color.Black);
 }
 
+static void GRIDTEST()
+{
+  for (int i = 0; i < 1600; i += 80)
+  {
+    Raylib.DrawLine(i, 0, i, 800, Color.Gray);
+
+  }
+  for (int i = 0; i < 800; i += 80)
+  {
+    Raylib.DrawLine(0, i, 1600, i, Color.Gray);
+
+  }
+}
+
+static List<TOWER> TOWERPLACE(List<TOWER> TOWERS)
+{
+  if (Raylib.IsMouseButtonPressed(MouseButton.Left))
+  {
+    Vector2 MouseFloat = Raylib.GetMousePosition();
+
+    int MouseIntX = 40 + 80 * (int)(Math.Floor(MouseFloat.X / 80)); //ganska klottrigt men detta avrundar så 
+    int MouseIntY = 40 + 80 * (int)(Math.Floor(MouseFloat.Y / 80)); //att det man endast kan placera enligt ett grid system
+
+    TOWERS.Add(new TOWER(MouseIntX, MouseIntY, 40));
+    
+  }
+  return TOWERS;
+}
+
+static void PREVIEW ()
+{
+  Vector2 MouseFloat = Raylib.GetMousePosition();
+
+  int MouseIntX = 40 + 80 * (int)(Math.Floor(MouseFloat.X / 80)); //ganska klottrigt men detta avrundar så 
+  int MouseIntY = 40 + 80 * (int)(Math.Floor(MouseFloat.Y / 80)); //att det man endast kan placera enligt ett grid system
+
+  Raylib.DrawCircle(MouseIntX, MouseIntY, 40, Color.Red);
+}
+
+static void TOWERDRAW(List<TOWER> TOWERS)
+{
+  for (int i = 0; i < TOWERS.Count; i++)
+  {
+    Raylib.DrawCircle(TOWERS[i].posX,TOWERS[i].posY,TOWERS[i].radius, Color.Black); 
+    Raylib.DrawCircle(TOWERS[i].posX,TOWERS[i].posY,TOWERS[i].radius - 5, Color.Blue); 
+  }
+}
--- Projekt/Square.cs
+++ Projekt/Square.cs
@@ -4,3 +4,18 @@ public class Square
 {
   public Rectangle rect;
 }
+
+public class TOWER
+{
+    public int posX;
+    public int posY;
+    public int radius;
+
+    // Constructor to initialize the TOWER object with posX, posY, and radius
+    public TOWER(int x, int y, int r)
+    {
+        posX = x;  
+        posY = y;  
+        radius = r;  
+    }
+}
